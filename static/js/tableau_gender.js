8306170231be2c5baf04c808b078de14df17a114
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-08-08 10:44:57 -0400

    troubleshooting tab server issue

--- static/js/tableau_gender.js
+++ static/js/tableau_gender.js
@@ -2,7 +2,7 @@ function initializeViz() {
   // JS object that points at empty div in the html
   var genderDiv = document.getElementById("tableauGender");
   // URL of the viz to be embedded
-  var url = "https://public.tableau.com/profile/shrilekha.vijayakanthan8354#!/vizhome/TechSalaryAnalysis/GenderAnalysis?publish=yes";
+  var url = "https://public.tableau.com/profile/shrilekha.vijayakanthan8354#!/vizhome/TechSalaryAnalysis/GenderAnalysis";
   // An object that contains options specifying how to embed the viz
   var options = {
     width: '600px',
@@ -11,3 +11,5 @@ function initializeViz() {
     hideToolbar: true,
   };
   viz = new tableau.Viz(genderDiv, url, options);
+
+initializeViz();
