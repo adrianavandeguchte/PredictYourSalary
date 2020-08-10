cfca9ce61a70a869056aa925b49dba757c49d6ae
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-08-10 08:47:54 -0400

    Update toolwords.js
    removed typescript, changed scaling

--- static/js/toolwords.js
+++ static/js/toolwords.js
@@ -1,6 +1,6 @@
 d3.json("/recommendations_data", function (err, words) {
 var scaleCount = d3.scale.linear()
-  .domain([10,5300])
+  .domain([50,5300])
   .range([15,100]);
 
 words.forEach(function (d) {
@@ -12,7 +12,10 @@ words = words.filter(function (d) {
 words = words.filter(function (d) {
   return d.recommended_first_language !== "Other"
 });
-console.log(words)
+words = words.filter(function (d) {
+  return d.recommended_first_language !== "TypeScript"
+});
+// console.log(words)
 // Encapsulate the word cloud functionality
 function wordCloud(selector) {
 
@@ -113,5 +116,5 @@ showNewWords(myWordCloud);
 });
 
 function selectFilter(filter) {
-  
+
 }
