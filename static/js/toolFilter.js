d141962506b31f90e20024781405781180de51b4
adrianavandeguchte <61213204+adrianavandeguchte@users.noreply.github.com>
2020-08-11 10:40:18 -0400

    Built new js script that redisplays both visualizations based on filter input

---
+++ static/js/toolFilter.js
@@ -0,0 +1,41 @@
+function loadFilter(filter) {
+  var recURL = "/recommendations_data" + filter
+  var toolURL = "/tools_data" + filter
+
+  console.log(recURL)
+
+  
+  d3.json(toolURL, function (err, toolData) {
+    toolData = toolData.filter(function (d) {
+      return d.tool !== "None"
+    });
+    toolData = toolData.filter(function (d) {
+      return d.tool !== "Other"
+    });
+    display(toolData)
+  });
+
+    d3.json(recURL, function (err, rawData) {
+      var scaleCount = d3.scale.linear()
+        .domain([50,5300])
+        .range([15,100]);
+      rawData.forEach(function (d) {
+        d.count = scaleCount(d.count);
+      });
+      words = words.filter(function (d) {
+        return d.recommended_first_language !== "None"
+      });
+      words = words.filter(function (d) {
+        return d.recommended_first_language !== "Other"
+      });
+      words = words.filter(function (d) {
+        return d.recommended_first_language !== "TypeScript"
+      });
+      wordDisplay(rawData);
+    });
+
+
+}
+
+
+loadFilter("/test_filter")
