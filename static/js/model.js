1cb3b3a5abd67bd29387667de047657f84d0ccfd
Sandy <60555203+sfroonjian@users.noreply.github.com>
2020-08-13 21:52:01 -0400

    got feature writing to appear

--- static/js/model.js
+++ static/js/model.js
@@ -51,24 +51,6 @@ function optionChangedTitle(value) {
     }
 }
 
-// function submitForm() {
-//   console.log("hi")
-//     var features = d3.select("#features");
-//     features.html("");
-//     features.html("n\
-//             Order of importance of the variables in predicting your salary:n\
-//             1. Country<br>n\
-//             2. Primary Dtabase<br>n\
-//             3. Years With This Database<br>n\
-//             4. Are You a Manager?<br>n\
-//             5. Is Education is Computer Related?<br>n\
-//             6. Years With This Type of Job<br>n\
-//             7. Amount of Telecommute Days Per Week<br>n\
-//             8. Employment Sector<br>n\
-//             9. Job Title<br>n\
-//             Change the factors around to see how your predicted salary will change!");
-// }
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
